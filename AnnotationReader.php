0b4aee5616cf76e4e74fbbbbfcb3f231285fdd14
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2023-05-26 19:41:35 +0200

    cs fix

--- AnnotationReader.php
+++ AnnotationReader.php
@@ -33,8 +33,8 @@ class AnnotationReader
 
     /**
      * Vérifie, instancie et renvoie la seule instance de la classe appelée.
-     *
-     * @return static
+	 *
+	 * @return static
      */
     public static function instance()
     {
@@ -66,9 +66,9 @@ class AnnotationReader
      * @param string                        $type  Un nom de classe/interface d'annotation facultatif - si spécifié, seules les annotations du type donné sont renvoyées.
      *                                             Alternativement, le préfixe avec "@" invoque la résolution de nom (vous permettant d'interroger par nom d'annotation.)
      *
-     * @return Annotation[] Instances d'annotation
-     *
      * @throws AnnotationException si un nom de classe donné n'est pas défini
+     *
+     * @return Annotation[] Instances d'annotation
      */
     public static function fromClass($class, ?string $type = null)
     {
@@ -83,9 +83,9 @@ class AnnotationReader
      * @param string                                         $type   Un nom facultatif de classe/d'interface d'annotation - si spécifié, seules les annotations du type donné sont renvoyées.
      *                                                               Alternativement, le préfixe avec "@" invoque la résolution de nom (vous permettant d'interroger par nom d'annotation.)
      *
-     * @return IAnnotation[] liste des objets Annotation
-     *
      * @throws AnnotationException pour une méthode ou un nom de classe non défini
+     *
+     * @return IAnnotation[] liste des objets Annotation
      */
     public static function fromMethod($class, ?string $method, ?string $type = null)
     {
@@ -100,9 +100,9 @@ class AnnotationReader
      * @param string                                           $type     Un nom de classe/interface d'annotation facultatif - si spécifié, seules les annotations du type donné sont renvoyées.
      *                                                                   Alternativement, le préfixe avec "@" invoque la résolution de nom (vous permettant d'interroger par nom d'annotation.)
      *
-     * @return IAnnotation[] liste des objets Annotation
-     *
      * @throws AnnotationException pour un nom de classe non défini
+     *
+     * @return IAnnotation[] liste des objets Annotation
      */
     public static function formProperty($class, ?string $property, ?string $type = null)
     {
